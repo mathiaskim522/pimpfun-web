c07fd32d1645af5155d8005e0dfd7d3992acd24e
mathiaskim522 <kimmathias522@gmail.com>
2025-02-14 19:07:38 +0900

    Add border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="container mx-auto">
       <div className="space-y-10 lg:space-y-0 px-4 py-20 block lg:grid grid-cols-1 auto-rows-auto lg:grid-cols-3 lg:grid-rows-3 gap-8 max-h-[1500px]">
-        <div className="bg-purple-900 rounded-2xl lg:row-span-2 relative overflow-hidden">
+        <div className="bg-purple-900 rounded-2xl lg:row-span-2 relative overflow-hidden border-purple-400 border-2">
           <Image
             src={"/hero-background.jpg"}
             width={800}
@@ -37,13 +37,13 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="bg-white rounded-2xl overflow-hidden lg:col-span-2">
+        <div className="bg-white rounded-2xl overflow-hidden lg:col-span-2 border-purple-400 border-2">
           <GirlsSection />
         </div>
-        <div className="bg-white rounded-2xl p-8 lg:row-span-2 lg:col-span-2">
+        <div className="bg-white rounded-2xl p-8 lg:row-span-2 lg:col-span-2 border-purple-400 border-2">
           <Leaderboard />
         </div>
-        <div className="bg-white rounded-2xl p-8 row-span-1">
+        <div className="bg-white rounded-2xl p-8 row-span-1 border-purple-400 border-2">
           <Social />
         </div>
       </div>
